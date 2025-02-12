ed064f31e7a0f94202b9af7fba7f7758eadb4e1b
diredocks <26994007+diredocks@users.noreply.github.com>
2025-02-12 23:21:28 +0800

    ghostty: init

---
+++ home-manager/ghostty.nix
@@ -0,0 +1,24 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      font-family = "ComicShannsMono Nerd Font";
+      window-height = 35;
+      window-width = 115;
+      font-size = 15;
+      mouse-hide-while-typing = true;
+      cursor-style = "block";
+      cursor-style-blink = false;
+      shell-integration-features = "no-cursor";
+      window-theme = "dark";
+      theme = "tokyonight-storm";
+      gtk-single-instance = true;
+    };
+  };
+}
