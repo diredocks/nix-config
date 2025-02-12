ac0064e63bee0c0a8c07cac03ec0b653ee3d237e
diredocks <26994007+diredocks@users.noreply.github.com>
2025-02-12 22:54:34 +0800

    brave: cli args

--- home-manager/pkgs.nix
+++ home-manager/pkgs.nix
@@ -9,7 +9,15 @@
     qbittorrent
     inkscape
     obs-studio
-    brave
+    (brave.override {
+      commandLineArgs = [
+        "--ozone-platform-hint=auto"
+        "--enable-features=AcceleratedVideoDecodeLinuxGL"
+        "--enable-wayland-ime"
+        "--wayland-text-input-version=3"
+        "--enable-features=TouchpadOverscrollHistoryNavigation"
+      ];
+    })
     fastfetch
     hugo
     wireshark
