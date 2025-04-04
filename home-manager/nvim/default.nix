736def76af6185ce6440ed9b422ecb8fdde1da55
diredocks <26994007+diredocks@users.noreply.github.com>
2025-04-04 13:57:48 +0800

    lazyvim: python support

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -151,6 +151,7 @@ in {
           { import = "lazyvim.plugins.extras.lang.clangd" },
           { import = "lazyvim.plugins.extras.lang.rust" },
           { import = "lazyvim.plugins.extras.lang.typescript" },
+          { import = "lazyvim.plugins.extras.lang.python" },
           { import = "plugins" },
           -- treesitter handled by xdg.configFile."nvim/parser", put this line at the end of spec to clear ensure_installed
           { "nvim-treesitter/nvim-treesitter",
