15ff241aef3095d3a130bb254a30d55d7266e73d
diredocks <26994007+diredocks@users.noreply.github.com>
2025-05-11 12:26:32 +0800

    hosts/claw-jp: enable bbr

--- hosts/claw-jp/hardware/boot.nix
+++ hosts/claw-jp/hardware/boot.nix
@@ -37,5 +37,8 @@
     "virtio_balloon"
     "virtio_console"
     "virtio_rng"
+    "tcp_bbr"
   ];
+  boot.kernel.sysctl."net.ipv4.tcp_congestion_control" = "bbr";
+  boot.kernel.sysctl."net.core.default_qdisc" = "fq";
 }
