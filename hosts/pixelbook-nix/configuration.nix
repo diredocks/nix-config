47798090de0edc3c2d7f347a9109acc711f7f1d6
diredocks <26994007+diredocks@users.noreply.github.com>
2025-06-04 23:01:19 +0800

    hosts/pixelbook: add power-profiles-deamon

--- hosts/pixelbook-nix/configuration.nix
+++ hosts/pixelbook-nix/configuration.nix
@@ -46,6 +46,8 @@
 
   boot.kernelPackages = pkgs.linuxPackages_6_12;
 
+  services.power-profiles-daemon.enable = true;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
 }
