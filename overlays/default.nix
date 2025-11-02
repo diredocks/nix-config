f9bddbb34984797c27b22de63e36e0a453aad505
diredocks <26994007+diredocks@users.noreply.github.com>
2025-11-02 22:17:19 +0800

    remove xray overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,20 +4,8 @@
   additions = final: _prev: import ../pkgs final.pkgs;
 
   # This one contains whatever you want to overlay
-  # You can change versions, add patches, set compilation flags, anything really.
-  # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
     # wrangler = inputs.wrangler.packages.${final.system}.default;
-    xray = prev.xray.overrideAttrs (old: rec {
-      version = "25.4.30";
-      src = prev.fetchFromGitHub {
-        owner = "XTLS";
-        repo = "Xray-core";
-        rev = "v${version}";
-        hash = "sha256-qhZgf+HQXfmWNnwjgDZwcs1WR77HoqRXd4VZEwFKPWs=";
-      };
-      vendorHash = "sha256-4kAU+8YXrJOu1wyiuFuPWT0dHI3WzlXY9s7NKyI9r5U=";
-    });
     vimPlugins = prev.vimPlugins.extend (
       final': prev': {
         nvim-treesitter = prev'.nvim-treesitter.overrideAttrs (old: rec {
