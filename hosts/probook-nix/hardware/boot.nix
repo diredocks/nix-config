225d20f25c33122210555bda22dc17ac547c6a66
diredocks <26994007+diredocks@users.noreply.github.com>
2025-11-13 19:02:01 +0800

    rm archlinux from boot

--- hosts/probook-nix/hardware/boot.nix
+++ hosts/probook-nix/hardware/boot.nix
@@ -7,11 +7,5 @@
 }
 : {
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.extraEntries."arch.conf" = ''
-    title Archlinux
-    linux /vmlinuz-linux-lts
-    initrd /initramfs-linux-lts.img
-    options root=UUID=f6215e55-ec45-48d7-b403-df87b39efcfe rw
-  '';
   boot.initrd.systemd.enable = true;
 }
